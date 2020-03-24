8046e4ab0afce44fd782a22c5e4123ac0b319b0b
Shiryayev Dmitriy <shiryayevx@gmail.com>
2020-03-24 19:34:25 +0300

    add socket

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,36 @@
-import express from 'express';
+import express, { Response, Request, NextFunction } from 'express';
+import createError, { HttpError } from 'http-errors';
+import logger from 'morgan';
 import path from 'path';
+import http from 'http';
+import io from 'socket.io';
 import 'reflect-metadata';
 import { AppRouter } from './routes';
 import './controllers';
 
 const app = express();
+const server = http.createServer(app);
+export const socket = io(server);
 
+socket.on('connection', () => console.log('connection established'));
+
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(AppRouter.getInstance());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000);
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(createError(404));
+});
 
-export default app;
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  res.locals.message = err.message;
+  res.locals.error = err;
+
+  res.sendStatus(err.status || 500);
+});
+
+server.listen(3000);
+
+export default server;
--- src/controllers/Compiler.ts
+++ src/controllers/Compiler.ts
@@ -2,20 +2,16 @@ import prettier from 'prettier';
 import { Request, Response } from 'express';
 import { controller, post } from './decorators';
 import compile from '../compiler';
-import { constants } from 'http2';
+import { socket } from '../app';
 
 @controller('/compile')
 export class Compiler {
   @post('/vba')
   compile(request: Request, response: Response) {
     const { body } = request;
+    const compiled = compile(body);
 
-    try {
-      const compiled = compile(body);
-
-      response.send(prettier.format(compiled));
-    } catch (error) {
-      response.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR);
-    }
+    socket.emit('message', prettier.format(compiled));
+    response.sendStatus(200);
   }
 }
